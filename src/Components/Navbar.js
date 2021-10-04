d551c3f11d194419aaf808000f1e1e017b69fc8d
ashutosh verma <ashutoshverma5555@gmail.com>
2021-10-04 16:23:48 +0530

    Updated Nav-bar

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -6,15 +6,21 @@ const Navbar = () => {
     return (
         <>
             <div id="navbar">
-                <div className="logo">
-
-                </div>
-                <ul>
-                    <li className="item">
-                        <Link className="link" to="/compiler">Compiler</Link>
+                  <div className="logo">
+                    <p>LOGO</p>
+                  </div>
+                  <input type="checkbox" id="click"/>
+                  <label htmlFor="click" className="fa fa-bars menu-icon" ></label>
+                 <label htmlFor="click" className="fa fa-close close-icon"></label>
+                  <ul>
+                    <li className="nav-item">
+                        <Link className={`${link} active`} to="/Home">Home</Link>
                     </li>
                     <li className="nav-item">
-                        <Link className="link" to="/dashboard">Dashboard</Link>
+                        <Link className={`${link}`} to="/compiler">Compiler</Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link className={`${link}`} to="/dashboard">Dashboard</Link>
                     </li>
                 </ul>
             </div>
