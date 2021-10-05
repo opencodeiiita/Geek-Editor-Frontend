f327dcfed17794676c75473ba8e4af0fe1e29cc8
batflarrow <dwivedia2001@gmail.com>
2021-10-06 02:37:59 +0530

    Add logo in navbar

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,20 +1,20 @@
-import React, {useState} from 'react'
-import { Link } from 'react-router-dom'
-import "./Navbar.css"
-
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import "./Navbar.css";
+import logo from '../assets/images/Logo.png';
 const Navbar = () => {
-    const [link, setLink] = useState('link');  
+    const [link, setLink] = useState('link');
     return (
         <>
             <div id="navbar">
-                  <div className="logo">
-                    <p>LOGO</p>
-                  </div>
-                  <input type="checkbox" id="click"/>
-                  <label htmlFor="click" className="fa fa-bars menu-icon" ></label>
-                 <label htmlFor="click" className="fa fa-close close-icon"></label>
-                  <ul>
-                   
+                <div className="logo">
+                    <img className="logoImg" src={logo} alt="Logo" />
+                </div>
+                <input type="checkbox" id="click" />
+                <label htmlFor="click" className="fa fa-bars menu-icon" ></label>
+                <label htmlFor="click" className="fa fa-close close-icon"></label>
+                <ul>
+
                     <li className="nav-item">
                         <Link className={`${link} active`} to="/compiler">Compiler</Link>
                     </li>
@@ -24,7 +24,7 @@ const Navbar = () => {
                 </ul>
             </div>
         </>
-    )
-}
+    );
+};
 
-export default Navbar
+export default Navbar;
