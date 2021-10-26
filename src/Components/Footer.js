cd078217b0bb8dfe63a5f396587058503f8a022d
Shashwat Mittal <74846797+shashwat-mittal@users.noreply.github.com>
2021-10-26 21:53:47 +0530

    Improved Footer (#187)
    * Footer Fixed
    
    * Improved Footer

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -16,62 +16,61 @@ const Footer = (props) => {
               <div className="logo-details">
                 <h1 className="logo_name">OpenCode'21</h1>
               <br/>
-              <br/>
-                <p className="para">A month-long program starting in October for students to start their journey in the world of open source.
-                  The Only requirement being an enthusiastic heart to learn.</p>
               </div>
-              <br/>
-              <br/>
               <div className="media-icons">
                 <img src="../assets/images/Facebook.png" src={Facebook}/>
                 <img src="../assets/images/Twitter.png" src={Twitter}/>
-                <img src="../assets/images/Github.png" src={Git}/>
-                
-                
-              </div>
+                <img src="../assets/images/Github.png" src={Git}/>                
+              </div> 
             </div>
+            
             <div className="link-boxes">
+            
               <ul className="Mentors" id="Email" >
-                <li className="link_name" id="c1">CONTACT US :</li>
-                <div><li><Link to='#'>Priyal Saxena : iec2020123@iiita.ac.in</Link></li></div>
-                <hr/>
-                <div><li><Link to='#'>Mudit Agarwal : iit2020092@iiita.ac.in</Link></li></div>
-                <hr/>
-                <div><li><Link to='/#'>Akshaj Kansal : iit2020245@iiita.ac.in</Link></li></div>
-                
+                <li className="link_name">CONTACT US :</li>
+                <div className="contact-flex">
+                <div>
+                <div>Priyal Saxena</div> 
+                <div>iec2020123@iiita.ac.in</div>
+                </div>
+                <div>
+                <div>Mudit Agarwal</div> 
+                <div>iit2020092@iiita.ac.in</div>
+                </div>
+                <div>
+                <p>Akshaj Kansal</p> 
+                <p>iit2020245@iiita.ac.in</p>
+                </div>
+                </div>
               </ul>
-              <ul className="Mentors" id="contents">
+              <div className="explore">
+              <div className="Mentors" id="contents">
                 <li className="link_name">EXPLORE</li>
                 <li><Link to='/#'>Home</Link></li>
                 <li><Link to='/#'>About</Link></li>
                 <li><Link to='/#'>Team</Link></li>
                 
               
-              </ul>
-              <ul className="Mentors" >
+              </div>
+              <div className="Mentors" >
                 <li className="link_name">LEGALS</li>
                 <li><Link to='/#'>Contact us</Link></li>
                 <li><Link to='/#'>Team</Link></li>
                 <li><Link to='/#'>Privacy</Link></li>
-              </ul>
-          <ul className="Mentors" >                                
+              </div>
+          <div className="Mentors" >                                
                 <li className="link_name">JOIN US</li>
                 <li><Link to='/https://discord.com/invite/PX7uJCSXPw'>Discord</Link></li>
-                <li><Link to='/https://devfolio.co/opencode/dashboard'>Discord</Link></li>
-                <br/>
-                <li><Link to='/https://opencodeiiita.github.io/'><button id="button"> VISIT US </button></Link></li>
-              </ul> 
-            </div>
-          </div>
-          <hr/>
-              <div className="bottom-details">
-              <div className="bottom_text">
-                <span className="copyright_text">Copyright©2021 <Link to='/https://geek-editor-frontend.herokuapp.com/compiler'>Geek Editor.</Link> </span>
-                <span className="policy_terms">                
-                  <img src={logo} height="55" alt="Logo"/>
-                </span>
+                <li><Link to='/https://devfolio.co/opencode/dashboard'>DevFolio</Link></li>
+                <li><Link to='/https://opencodeiiita.github.io/'>VISIT US</Link></li>
+            </div> 
               </div>
+              
             </div>
+            <br/>
+            <div className="copyright_text">Copyright©2021 Geek Editor</div>
+          </div>
+              
         </footer>
         
     );
